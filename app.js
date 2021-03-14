0eb3551649e3e9167834739424a9824276dbe01d
const.kamil.lazarski@gmail.com <const.kamil.lazarski@gmail.com>
2021-03-14 22:38:50 +0100

    done

---
+++ app.js
@@ -0,0 +1,26 @@
+{ 
+  const preloader = document.querySelector('.hero__preloader');
+  const hero = document.querySelector('.hero');
+
+  let load = 0;
+
+  const blurring = () => {
+    load++
+
+    if (load > 99) {
+      clearInterval(int)
+    }
+    
+    preloader.innerText = `${load}%`;
+    preloader.style.opacity = scale(load, 0, 100, 1, 0);
+    hero.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+
+  }
+
+  const scale = (num, in_min, in_max, out_min, out_max) => {
+      return (( num - in_min ) * (out_max - out_min)) / ( in_max - in_min) + out_min; 
+  };
+
+  let int = setInterval(blurring, 30);
+
+}
